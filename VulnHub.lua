3d526db0fc4908ed135d6923492202ee14f53ac0
leos00281-eng <leos00281@gmail.com>
2026-08-31 00:44:56 +0800

    Update VulnHub.lua

--- VulnHub.lua
+++ VulnHub.lua
@@ -1,7 +1,8 @@
-local queueFunction = syn and syn.queue_on_teleport or queue_on_teleport or flux and flux.queue_on_teleport
+local _0x1a = syn and syn.queue_on_teleport or queue_on_teleport or flux and flux.queue_on_teleport
+local _0x2b = "https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/VulnHub.lua"
 
-if queueFunction then
-    queueFunction([[
+if _0x1a then
+    _0x1a([[
         task.spawn(function()
             if not game:IsLoaded() then
                 game.Loaded:Wait()
@@ -19,6 +20,6 @@ task.spawn(function()
         game.Loaded:Wait()
     end
     pcall(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/VulnHub.lua"))()
+        loadstring(game:HttpGet(_0x2b))()
     end)
 end)
