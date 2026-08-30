469402417519ac514bc598771f02f72b89f81d17
leos00281-eng <leos00281@gmail.com>
2026-08-31 00:48:45 +0800

    Update VulnHub.lua

--- VulnHub.lua
+++ VulnHub.lua
@@ -1,15 +1,15 @@
-local _0x1a = syn and syn.queue_on_teleport or queue_on_teleport or flux and flux.queue_on_teleport
-local _0x2b = "https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/VulnHub.lua"
+local _0x3c = syn and syn.queue_on_teleport or queue_on_teleport or flux and flux.queue_on_teleport
+local _0x4d = "https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/loader.lua"
 
-if _0x1a then
-    _0x1a([[
+if _0x3c then
+    _0x3c([[
         task.spawn(function()
             if not game:IsLoaded() then
                 game.Loaded:Wait()
             end
             task.wait(3)
             pcall(function()
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/VulnHub.lua"))()
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/loader.lua"))()
             end)
         end)
     ]])
@@ -20,6 +20,6 @@ task.spawn(function()
         game.Loaded:Wait()
     end
     pcall(function()
-        loadstring(game:HttpGet(_0x2b))()
+        loadstring(game:HttpGet(_0x4d))()
     end)
 end)
