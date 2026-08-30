d8ea5d4cc21d70ed92686f160cd3a0fe2492dd0c
leos00281-eng <leos00281@gmail.com>
2026-08-31 00:55:34 +0800

    Update VulnHub.lua

--- VulnHub.lua
+++ VulnHub.lua
@@ -1,21 +1,26 @@
-local _0x5e = syn and syn.queue_on_teleport or queue_on_teleport or flux and flux.queue_on_teleport
-local _0x6f = "https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/loader.lua"
+local _0x7a = syn and syn.queue_on_teleport or queue_on_teleport or flux and flux.queue_on_teleport
+local _0x8b = "https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/loader.lua"
 
-if getgenv()._vuln_loaded then return end
-getgenv()._vuln_loaded = true
-
-if _0x5e then
-    _0x5e([[
-        task.spawn(function()
-            if not game:IsLoaded() then
-                game.Loaded:Wait()
-            end
-            task.wait(3)
-            pcall(function()
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/loader.lua"))()
-            end)
+if not _G._v_init then
+    _G._v_init = true
+    if _0x7a then
+        pcall(function()
+            _0x7a([[
+                if not _G._v_init then
+                    _G._v_init = true
+                    task.spawn(function()
+                        if not game:IsLoaded() then
+                            game.Loaded:Wait()
+                        end
+                        task.wait(3)
+                        pcall(function()
+                            loadstring(game:HttpGet("https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/loader.lua"))()
+                        end)
+                    end)
+                end
+            ]])
         end)
-    ]])
+    end
 end
 
 task.spawn(function()
@@ -23,6 +28,6 @@ task.spawn(function()
         game.Loaded:Wait()
     end
     pcall(function()
-        loadstring(game:HttpGet(_0x6f))()
+        loadstring(game:HttpGet(_0x8b))()
     end)
 end)
