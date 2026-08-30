b2886743f85ac59c9ad90bbb9b80eae27807f3d7
leos00281-eng <leos00281@gmail.com>
2026-08-31 02:22:50 +0800

    Update and rename VulnHub.lua1 to VulnHub.lua

---
+++ VulnHub.lua
@@ -0,0 +1,24 @@
+local queueFunction = syn and syn.queue_on_teleport or queue_on_teleport or flux and flux.queue_on_teleport
+
+if queueFunction then
+    queueFunction([[
+        task.spawn(function()
+            if not game:IsLoaded() then
+                game.Loaded:Wait()
+            end
+            task.wait(3)
+            pcall(function()
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/VulnHub.lua"))()
+            end)
+        end)
+    ]])
+end
+
+task.spawn(function()
+    if not game:IsLoaded() then
+        game.Loaded:Wait()
+    end
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/VulnHub.lua"))()
+    end)
+end)
