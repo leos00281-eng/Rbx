c7127139f97c67188e8eb51bcd9c67109cfbdae9
leos00281-eng <leos00281@gmail.com>
2026-08-31 00:39:48 +0800

    Update VulnHub.lua

--- VulnHub.lua
+++ VulnHub.lua
@@ -1,44 +1,24 @@
-local _0x1a = syn and syn.queue_on_teleport or queue_on_teleport or (flux and flux.queue_on_teleport) or (fluxus and fluxus.queue_on_teleport)
+local queueFunction = syn and syn.queue_on_teleport or queue_on_teleport or flux and flux.queue_on_teleport
 
-local _0x2b = "https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/VulnHub.lua"
-
-if _0x1a then
-    _0x1a(string.format([[
+if queueFunction then
+    queueFunction([[
         task.spawn(function()
-            if not game:IsLoaded() then game.Loaded:Wait() end
-            
-            local _0x3c = syn and syn.queue_on_teleport or queue_on_teleport or (flux and flux.queue_on_teleport) or (fluxus and fluxus.queue_on_teleport)
-            if _0x3c then
-                _0x3c(_G.__VULN_QUEUE_STRING or "")
+            if not game:IsLoaded() then
+                game.Loaded:Wait()
             end
-            
             task.wait(3)
             pcall(function()
-                loadstring(game:HttpGet('%s'))()
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/VulnHub.lua"))()
             end)
         end)
-    ]], _0x2b))
+    ]])
 end
-
-_G.__VULN_QUEUE_STRING = string.format([[
-    local _0x3c = syn and syn.queue_on_teleport or queue_on_teleport or (flux and flux.queue_on_teleport) or (fluxus and fluxus.queue_on_teleport)
-    if _0x3c then
-        _0x3c(_G.__VULN_QUEUE_STRING or "")
-    end
-    task.spawn(function()
-        if not game:IsLoaded() then game.Loaded:Wait() end
-        task.wait(3)
-        pcall(function()
-            loadstring(game:HttpGet('%s'))()
-        end)
-    end)
-]], _0x2b)
 
 task.spawn(function()
     if not game:IsLoaded() then
         game.Loaded:Wait()
     end
     pcall(function()
-        loadstring(game:HttpGet(_0x2b))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/VulnHub.lua"))()
     end)
 end)
