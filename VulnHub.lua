5404648d42594248e17fb226d084592a656d05bd
leos00281-eng <leos00281@gmail.com>
2026-08-31 11:03:05 +0800

    Update VulnHub.lua

--- VulnHub.lua
+++ VulnHub.lua
@@ -2,7 +2,7 @@
 local queueOnTeleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 if queueOnTeleport then
     pcall(function()
-        queueOnTeleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/loader.lua"))()')
+        queueOnTeleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/leos00281-eng/Rbx/refs/heads/main/VulnHub.lua"))()')
     end)
 end
 
